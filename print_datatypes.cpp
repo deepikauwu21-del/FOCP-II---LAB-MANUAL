5f99299bf3f6bdfc4c1ac51fb42885ab958846f2
deepikauwu21-del <deepika.uwu.21@gmail.com>
2026-01-20 23:00:22 +0530

    Add files via upload

---
+++ print_datatypes.cpp
@@ -0,0 +1,9 @@
+//priting int and float value 
+#include <iostream>
+int main(){
+    int a = 10 ;
+    float f = 10.5;
+    char c = 'A';
+    std::cout<<"the value of a="<<a<<std::endl<<"the value of f="<<f<<std::endl<<"the value of c="<<c;
+    return 0;
+}
